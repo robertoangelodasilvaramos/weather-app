3ee1aa6d2385fe6b25083421f4bd0bdb77e59b3e
roberto.ramos <roberto.angelo@e-unicred.com.br>
2024-09-11 01:04:15 -0300

    inicial

--- src/app/modules/weather/components/weather-card/weather-card.component.ts
+++ src/app/modules/weather/components/weather-card/weather-card.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { WeatherDatas } from '../../../../models/interfaces/WeatherDatas';
 
 @Component({
   selector: 'app-weather-card',
   templateUrl: './weather-card.component.html',
   styleUrls: []
 })
-export class WeatherCardComponent {
+export class WeatherCardComponent implements OnInit {
+
+  @Input() weatherDatasInput!: WeatherDatas;
+
+  ngOnInit(): void {
+    console.log("Dados recebidos do component pai :");
+    this.weatherDatasInput;
+  }
 
 }
